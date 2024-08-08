62975d457b30a68e2ce4738a157e7e3b6694f6cc
Egor Briliov <88632284+egorbriliov@users.noreply.github.com>
2024-08-08 04:47:08 +0300

    Improved view

--- leet code/TwoSum.py
+++ leet code/TwoSum.py
@@ -8,6 +8,7 @@ class Solution(object):
         :param target: int
         :return: list with index elements which gives sum equal to target
         """
+
         def search(new_list, target, nums):
             """
             :param new_list: list - where will be find numbers
@@ -22,9 +23,11 @@ class Solution(object):
                         return [nums.index(first_number), nums.index(number, nums.index(first_number) + 1)]
                         # Этот return завершает функцию search()
                 return search(new_list, target, nums)
+
         # Получается копия списка, для поиска индексов файлов
         main_nums = nums[:]
         return search(nums, target, main_nums)
+
 
 solution = Solution()
 
@@ -32,46 +35,16 @@ solution = Solution()
 class TestMyModule(unittest.TestCase):
 
     def test_1(self):
-        # Arrange
-        nums = [1, 2, 3]
-        sum_of_numbers = 3
-        # Act
-        actual_output = solution.twoSum(nums, sum_of_numbers)
-        # Assert
-        self.assertEqual(actual_output, [0, 1])
+        self.assertEqual(solution.twoSum(nums=[1, 2, 3], target=3), [0, 1])
 
     def test_2(self):
-        # Arrange
-        nums = [1, 2, 3, 6]
-        sum_of_numbers = 7
-        # Act
-        actual_output = solution.twoSum(nums, sum_of_numbers)
-        # Assert
-        self.assertEqual(actual_output, [0, 3])
+        self.assertEqual(solution.twoSum(nums=[1, 2, 3, 6], target=7), [0, 3])
 
     def test_3(self):
-        # Arrange
-        nums = [1, 4, 4, 6]
-        sum_of_numbers = 8
-        # Act
-        actual_output = solution.twoSum(nums, sum_of_numbers)
-        # Assert
-        self.assertEqual(actual_output, [1, 2])
+        self.assertEqual(solution.twoSum(nums=[1, 4, 4, 6], target=8), [1, 2])
 
     def test_4(self):
-        # Arrange
-        nums = [1, 3, 5, 6]
-        sum_of_numbers = 4
-        # Act
-        actual_output = solution.twoSum(nums, sum_of_numbers)
-        # Assert
-        self.assertEqual(actual_output, [0, 1])
+        self.assertEqual(solution.twoSum(nums=[1, 3, 5, 6], target=4), [0, 1])
 
     def test_5(self):
-        # Arrange
-        nums = [1, 3, 5, 6]
-        sum_of_numbers = 11
-        # Act
-        actual_output = solution.twoSum(nums, sum_of_numbers)
-        # Assert
-        self.assertEqual(actual_output, [2, 3])
+        self.assertEqual(solution.twoSum(nums=[1, 3, 5, 6], target=11), [2, 3])
