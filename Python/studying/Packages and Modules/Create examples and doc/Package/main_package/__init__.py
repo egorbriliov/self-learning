aac4163e48fde7ff9f9e9934a26fdf8bb05b54dd
Egor Briliov <88632284+egorbriliov@users.noreply.github.com>
2024-10-25 03:46:20 +0300

    Added new comments

---
+++ Python/studying/Packages and Modules/Create examples and doc/Package/main_package/__init__.py
@@ -0,0 +1,2 @@
+""" Works as method __init__ in class. When something from package will be imported file will be started."""
+__all__ = []
---
+++ Python/studying/Packages and Modules/Create examples and doc/Package/main_package/lower_package/__init__.py
@@ -0,0 +1,1 @@
+# Works as method __init__ in class. When something from package will be imported file will be started.
