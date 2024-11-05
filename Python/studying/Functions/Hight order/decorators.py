3028e0f75b9debf83a1a0bd173613df9550822cf
Egor Briliov <88632284+egorbriliov@users.noreply.github.com>
2024-11-05 13:06:15 +0300

    File was reworked

--- Python/studying/Functions/Hight order/decorators.py
+++ Python/studying/Functions/Hight order/decorators.py
@@ -1,3 +1,20 @@
+from datetime import datetime
+
+# Decorator with using *args and **kwargs
+def decorator(func):
+    def wrapper(*args, **kwargs):
+        func(*args, **kwargs)
+    return wrapper
+
+@decorator
+def example(stroke: str, times: int = 1):
+    for _ in range(times):
+        print(stroke)
+
+# example("args + kwargs", times=2)
+
+
+# Decorator with parameters for decorator
 def repeat(times: int):
     def decorator(func):
         def wrapper(*args, **kwargs):
@@ -7,7 +24,7 @@ def repeat(times: int):
         return wrapper
     return decorator
 
-@repeat(3)
+@repeat(times=3)
 def greet(name: str) -> str:
     """Creating greeting stroke with name
     
@@ -24,4 +41,5 @@ def greet(name: str) -> str:
     """
     return f"Hello, {name}!"
 
-greet("Alex")
+# greet("Alex")
+
